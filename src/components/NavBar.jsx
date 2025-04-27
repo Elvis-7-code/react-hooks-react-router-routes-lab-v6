11ca130853bf3cad22e98beaaf4049420125e50e
Elvis <elvis.wachira@student.moringaschool.com>
2025-04-27 12:43:12 +0300

    Home-Navlink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,11 @@ import "./NavBar.css";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
-    </nav>
+    <div className="navbar" role="navigation">
+      <Navlink to="/">Home</Navlink>
+      <Navlink to=""></Navlink>
+      <Navlink></Navlink>
+    </div>
     );
 };
 
